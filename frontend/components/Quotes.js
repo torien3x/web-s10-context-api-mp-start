c8e644358a9edf432f48c327ee9645366a307b4b
Torien3x <tstanley0323@gmail.com>
2024-02-14 22:25:24 -0700

    s10 context api uodate

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { QuotesContext } from '../context/quotesContext'
 
-export default function Quotes(props) {
+export default function Quotes() {
   const {
     quotes,
     highlightedQuote,
@@ -9,7 +10,7 @@ export default function Quotes(props) {
     editQuoteAuthenticity,
     setHighlightedQuote,
     toggleVisibility,
-  } = props
+  } = useContext(QuotesContext)
   return (
     <div id="quotes">
       <h3>Quotes</h3>
